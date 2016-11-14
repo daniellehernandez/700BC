46c2a887c7e89a18174f4cf39ba53049477125ee
Danielle Hernandez <dmhernandez@up.edu.ph>
2016-11-14 15:46:44 +0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -7,17 +7,20 @@ public class Game extends StateBasedGame{
 	public static final String Name = "Naked Hermes";
 	public static final int Menu = 0;
 	public static final int PlayScreen = 1;
+	public static final int Parthenon = 3;
 
 	public Game (String Name){
 		super(Name);
 		this.addState(new Menu(Menu));
 		this.addState(new PlayScreen(PlayScreen));
+		this.addState(new Parthenon(Parthenon));
 	}
 
 
 	public void initStatesList(GameContainer gc) throws SlickException {
 		this.getState(Menu).init(gc,this);
 		this.getState(PlayScreen).init(gc,this);
+		this.getState(Parthenon).init(gc,this);
 		this.enterState(Menu);
 	}
 
