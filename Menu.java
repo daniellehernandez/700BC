8807152cde4dcb8420fe9a6b742af9bbb5889e41
Danielle Hernandez <dmhernandez@up.edu.ph>
2016-11-14 15:54:31 +0800

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,41 +1,40 @@
+import org.lwjgl.input.Mouse;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
-
-public class Game extends StateBasedGame{
-
-	public static final String Name = "Naked Hermes";
-	public static final int Menu = 0;
-	public static final int PlayScreen = 1;
-	public static final int Parthenon = 3;
-
-	public Game (String Name){
-		super(Name);
-		this.addState(new Menu(Menu));
-		this.addState(new PlayScreen(PlayScreen));
-		this.addState(new Parthenon(Parthenon));
+public class PlayScreen extends BasicGameState{
+	Image hermes;
+	int x = 10;
+	int y = 300;
+	
+	public PlayScreen(int state){
+		
 	}
 
 
-	public void initStatesList(GameContainer gc) throws SlickException {
-		this.getState(Menu).init(gc,this);
-		this.getState(PlayScreen).init(gc,this);
-		this.getState(Parthenon).init(gc,this);
-		this.enterState(Menu);
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		
 	}
 
-	public static void main(String[] args) {
-		AppGameContainer appgc;
 
-		try{
-			appgc = new AppGameContainer(new Game(Name));
-			appgc.setDisplayMode(700,500,false);
-			appgc.start();
-		}catch(SlickException e){ 
-			e.printStackTrace(); 
-		}
-
-
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		Image bg = new Image("res/Map.png");
+		g.drawImage(bg,0,0);
+		
 	}
 
+
+	public void update(GameContainer gc, StateBasedGame sbg, int delta)throws SlickException {
+		Input input = gc.getInput();
+
+
+			if(input.isKeyPressed(Input.KEY_SPACE)){
+				sbg.enterState(3);
+			}
+	}
+
+
+	public int getID() {
+		return 1;
+	}
 }
