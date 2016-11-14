5e88ac361bacbfdb584f2300ad2d3a1f2bd20fbb
Danielle Hernandez <dmhernandez@up.edu.ph>
2016-11-14 15:47:17 +0800

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,42 +1,41 @@
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
-import org.lwjgl.input.Mouse;
 
-public class Menu extends BasicGameState{
-	
-	
-	public Menu(int state){
-		
+
+public class Game extends StateBasedGame{
+
+	public static final String Name = "Naked Hermes";
+	public static final int Menu = 0;
+	public static final int PlayScreen = 1;
+	public static final int Parthenon = 3;
+
+	public Game (String Name){
+		super(Name);
+		this.addState(new Menu(Menu));
+		this.addState(new PlayScreen(PlayScreen));
+		this.addState(new Parthenon(Parthenon));
 	}
 
 
-	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-		
+	public void initStatesList(GameContainer gc) throws SlickException {
+		this.getState(Menu).init(gc,this);
+		this.getState(PlayScreen).init(gc,this);
+		this.getState(Parthenon).init(gc,this);
+		this.enterState(Menu);
 	}
 
+	public static void main(String[] args) {
+		AppGameContainer appgc;
 
-	//WHERE YOU PUT THE GRAPHICS THAT WILL SHOW ON THE SCREEN
-	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-		Image bg = new Image("res/bg.png");
-		g.drawImage(bg,0,0);
-
-	}
-
-
-	public void update(GameContainer gc, StateBasedGame sbg, int delta)throws SlickException {
-		Input input = gc.getInput();
-		int xMouse = Mouse.getX();
-		int yMouse = Mouse.getY();
-
-		if((xMouse > 488 && xMouse < 633)&& (yMouse > 136 && yMouse < 187 )){ //
-			if(input.isMouseButtonDown(0)){
-				sbg.enterState(1);
-			}
+		try{
+			appgc = new AppGameContainer(new Game(Name));
+			appgc.setDisplayMode(700,500,false);
+			appgc.start();
+		}catch(SlickException e){ 
+			e.printStackTrace(); 
 		}
-	}
 
 
-	public int getID() {
-		return 0;
 	}
+
 }
