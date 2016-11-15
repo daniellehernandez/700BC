41a81b86465556be5fc5529694b08e3b13e556f3
Jay Vince Serato <jdserato@up.edu.ph>
2016-11-15 16:56:58 +0800

    Apollo can now move within bounds and is amazing

--- Parthenon.java
+++ Parthenon.java
@@ -10,7 +10,11 @@ public class Parthenon extends BasicGameState {
     private SpriteSheet apollo;
     private Animation apolloAnimate;
     int x = 10;
-    int y = 300;
+    int y = 250;
+    boolean move = false;
+    boolean direction = false;
+    boolean firstTimeOnLeft = false;
+    boolean firstTimeOnRight = false;
 
     public Parthenon(int state){
 
@@ -18,27 +22,59 @@ public class Parthenon extends BasicGameState {
 
 
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-        apollo = new SpriteSheet("res/apollospritesheet.png",350,250);
-        apolloAnimate = new Animation(apollo, 300);
-        apolloAnimate.getFrame();
+        apollo = new SpriteSheet("res/Apollo.png",350,250);
+        apolloAnimate = new Animation(apollo, 150);
+
     }
 
 
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+        int frame = 0;
         Image bg = new Image("res/Parthenon.png");
+        Image apolloConstant = new Image("res/ApolloWalk2.png");
         g.drawImage(bg,0,0);
-        apolloAnimate.draw(0,0);
-
+        if(move == true) {
+            apolloAnimate.getCurrentFrame().getFlippedCopy(direction, false).draw(x, y);
+            move = false;
+        } else {
+            g.drawImage(apolloConstant.getFlippedCopy(direction,false),x,y);
+        }
     }
 
 
     public void update(GameContainer gc, StateBasedGame sbg, int delta)throws SlickException {
         apolloAnimate.update(delta);
         Input input = gc.getInput();
-        if(input.isKeyDown(Input.KEY_UP)){y -= 1;}
-        if(input.isKeyDown(Input.KEY_DOWN)){y += 1;}
-        if(input.isKeyDown(Input.KEY_LEFT)){x -= 1;}
-        if(input.isKeyDown(Input.KEY_RIGHT)){x += 1;}
+        //if(input.isKeyDown(Input.KEY_UP)){y -= 1;}
+        //if(input.isKeyDown(Input.KEY_DOWN)){y += 1;}
+        if(input.isKeyDown(Input.KEY_LEFT) && x > -200) {
+            if (x > 500) {
+                x = 500;
+            }
+            direction = true;
+            move = true;
+            firstTimeOnRight = true;
+            if (firstTimeOnLeft) {
+                x -= 70;
+                firstTimeOnLeft = false;
+            } else {
+                x -= 1;
+            }
+        }
+        if(input.isKeyDown(Input.KEY_RIGHT) && x < 550){
+            if (x < -100) {
+                x = -100;
+            }
+            direction = false;
+            move = true;
+            firstTimeOnLeft = true;
+            if (firstTimeOnRight) {
+                x += 70;
+                firstTimeOnRight = false;
+            } else {
+                x += 1;
+            }
+        }
     }
 
 
