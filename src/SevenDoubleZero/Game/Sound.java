a6b48bf891c937d8a2632cfbd50aad7c5b1837ad
Jay Vince Serato <jdserato@up.edu.ph>
2016-11-23 11:06:00 +0800

    Delete Sound.java

--- src/SevenDoubleZero/Game/Sound.java
+++
@@ -1,9 +0,0 @@
-package SevenDoubleZero.Game;
-
-import java.applet.Applet;
-import java.applet.AudioClip;
-
-public class Sound{
-    public static final AudioClip thunder = Applet.newAudioClip(Sound.class.getResource("res/Sounds/thunder.wav"));
-
-}
