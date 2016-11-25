806454bf7fb47062c3927c82b732d83e78ed5985
Danielle Hernandez <dmhernandez@up.edu.ph>
2016-11-25 17:26:08 +0800

    Update Parthenon.java

--- src/SevenDoubleZero/Game/Parthenon.java
+++ src/SevenDoubleZero/Game/Parthenon.java
@@ -1,11 +1,11 @@
-package SevenDoubleZero.Game;
+package Game;
 
-import SevenDoubleZero.Characters.Apollo;
-import SevenDoubleZero.Characters.Athena;
-import SevenDoubleZero.Characters.RPGCharacter;
+import Characters.*;
 import org.newdawn.slick.*;
+import org.newdawn.slick.Game;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.Sound;
 
 import javax.swing.*;
 
@@ -13,6 +13,10 @@ class Parthenon extends BasicGameState {
     private RPGCharacter player;
     private RPGCharacter ai;
     private Animation bg;
+    private Music bgMusic2;
+    private Sound walk;
+    private Sound attack;
+    private Sound hurt;
 
     Parthenon() {
     }
@@ -21,6 +25,11 @@ class Parthenon extends BasicGameState {
         player = new Apollo();
         ai = new Athena(350, true);
         bg = new Animation(new SpriteSheet("res/Maps/Parthenons.png", 700, 500), 1500);
+        bgMusic2 = new Music("res/Sounds/gamebg.wav");
+        attack = new Sound("res/Sounds/attack.wav");
+        walk = new Sound("res/Sounds/walkk.wav");
+        hurt = new Sound("res/Sounds/hurt.wav");
+
     }
 
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
@@ -28,6 +37,8 @@ class Parthenon extends BasicGameState {
         g.drawString("Your HP: " + player.getHealth(), 50, 10);
         g.drawString("Enemy HP: " + ai.getHealth(), 250, 10);
         JFrame frame = new JFrame("Game Over");
+        Input input = gc.getInput();
+
 
         if (player.atk) {
             if (player.charATK.getCurrentFrame().equals(player.charATK.getImage(3))) {
@@ -80,6 +91,7 @@ class Parthenon extends BasicGameState {
                 ai.attacked = true;
             }
             else if (ai.charATK.getCurrentFrame().equals(ai.charATK.getImage(4)) && ai.attacked) {
+                if(!hurt.playing()){ hurt.play();} //SOUND FX FOR HURT 
                 player.takeDamage((float) 20);
                 ai.attacked = false;
             }
@@ -141,6 +153,7 @@ class Parthenon extends BasicGameState {
         Input input = gc.getInput();
 
         if (input.isKeyDown(Input.KEY_A)) {
+            if(!attack.playing()){ attack.play();} //SOUND FX
             player.atk = true;
             ai.atk = true;
         }
@@ -150,6 +163,7 @@ class Parthenon extends BasicGameState {
             player.onceJumped = true;
         }
         if (input.isKeyDown(Input.KEY_LEFT) && player.x > -200) {
+            if(!walk.playing()){ walk.play();} // SOUND FX
             if (player.x > 500) {
                 player.x = 500;
                 player.realX = 500;
@@ -165,7 +179,9 @@ class Parthenon extends BasicGameState {
                 player.realX--;
             }
         }
+
         if (input.isKeyDown(Input.KEY_RIGHT) && player.x < 550) {
+            if(!walk.playing()){ walk.play();} //SOUND FX
             if (player.x < -100) {
                 player.x = -100;
                 player.realX = -100;
@@ -178,16 +194,37 @@ class Parthenon extends BasicGameState {
                 player.firstTimeOnRight = false;
             } else {
                 player.x++;
+
                 player.realX++;
             }
         }
 
-        ai.atk = ai.isNear(player, 40);
+
+       ai.atk = ai.isNear(player, 40);
         ai.move = !(ai.isNear(player, 150));
     }
 
 
     public int getID() {
         return 3;
+    }
+
+    public void enter(GameContainer gc, StateBasedGame sbg){ //BG MUSIC REQUIRED
+        Input input = gc.getInput();
+        try{
+            super.enter(gc, sbg);
+        }catch (SlickException e){
+            e.printStackTrace();
+        }
+        bgMusic2.loop();
+    }
+
+    public void leave(GameContainer gc, StateBasedGame sbg){ //BG MUSIC REQUIRED
+        try{
+            super.enter(gc, sbg);
+        }catch (SlickException e){
+            e.printStackTrace();
+        }
+        bgMusic2.stop();
     }
 }
